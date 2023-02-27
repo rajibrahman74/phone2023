75d81b7db97d208fd11a74d5fb20eb460d6ad556
rajibrahman74e <rajiburrahman74@gmail.com>
2023-02-27 20:53:27 +0600

    [ADD]-displayd-phone-in-ui

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 // phone data load with function
 
-const loadPhones = async () => {
-  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhones = async (searchFieldValue) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldValue}`;
   const res = await fetch(url);
   const data = await res.json();
-  displayPhones(data.data[0]);
+  displayPhones(data.data);
 };
+
 
 
 
@@ -13,13 +14,38 @@ const loadPhones = async () => {
 
 const displayPhones = (phones) => {
   console.log(phones);
-  const phoneContainer = document.getElementById("phones-container");
+  const phonesContainer = document.getElementById("phones-container");
+  phonesContainer.innerText = '';
 
-  phones.forEach(phone => {
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
 
-    
+    phoneDiv.innerHTML = `
+    <div class="card p-2">
+        <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+    </div>
+    `;
+    phonesContainer.appendChild(phoneDiv);
   });
 };
+
+
+
+// search phone field with function
+
+document.getElementById("search-btn").addEventListener("click", function () {
+  const searchField = document.getElementById("search-field");
+  const searchFieldValue = searchField.value;
+  loadPhones(searchFieldValue);
+});
+
+
 
 
 loadPhones();
