c42a91233dee060fcb526b21e49d51efe9b68c79
rajibrahman74e <rajiburrahman74@gmail.com>
2023-02-27 21:30:01 +0600

    [ADD]-no-phone-found-validaation

--- js/app.js
+++ js/app.js
@@ -13,10 +13,25 @@ const loadPhones = async (searchFieldValue) => {
 // display phones with function
 
 const displayPhones = (phones) => {
+  
   console.log(phones);
   const phonesContainer = document.getElementById("phones-container");
   phonesContainer.innerText = '';
 
+  // display only 15 phones
+  phones = phones.slice(0, 15);
+
+  // no-found message
+  const noPhoneFound = document.getElementById("no-found-message");
+  if (phones.length === 0) {
+    noPhoneFound.classList.remove("d-none");
+  } else {
+    noPhoneFound.classList.add("d-none");
+  }
+
+
+
+  // display all phones in UI
   phones.forEach((phone) => {
     const phoneDiv = document.createElement("div");
     phoneDiv.classList.add("col");
@@ -48,4 +63,4 @@ document.getElementById("search-btn").addEventListener("click", function () {
 
 
 
-loadPhones();
+// loadPhones();
